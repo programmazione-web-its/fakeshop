c1166d0333482b0b130e229962e2174ef1d806c8
analayze <annalisa@antonioli.dev>
2025-10-20 20:47:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import products from './data/products.json'
+
+function App() {
+  return (
+    <>
+      <h1>Fakeshop</h1>
+      <footer className='bg-secondary  rounded-t-xl mt-20'>
+        <div className='container text-primary text-md text-center py-20 '>
+          Copyright © {new Date().getFullYear()} - Fakeshop. All right reserved
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
