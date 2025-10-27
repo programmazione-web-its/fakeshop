505eca7ba2d6f9fb57b63673bb27ffa5d51b0023
Annalisa Antonioli <a.antonioli@edu-its.it>
2025-10-27 14:07:45 +0100

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,25 +2,28 @@ import { useContext } from 'react'
 import CartContext from '../store/cart-context'
 
 function Product({ product }) {
-  const { id, title, price, image } = product
+  const { id, title, price, image, description } = product
   const { addItemToCart } = useContext(CartContext)
   return (
-    <div id={id} className='product'>
-      <div className='w-full aspect-square mb-6'>
+    <div id={id} className='product h-full relative '>
+      <div className='w-full aspect-square mb-6 shrink-0 grow-0'>
         <img
           src={image}
           alt={title}
           className='w-full h-full object-contain object-center m-auto'
         />
       </div>
-      <h2>{title}</h2>
-      <p>{price}</p>
+      <div className='bg-primary rounded-t-lg p-4 flex flex-col'>
+      <h2 className='text-2xl text-secondary font-bold'>{title}</h2>
+      <p className='text-white text-xl my-2 font-bold'>€ {price}</p>
+      <p className='text-white'>{description}</p>
       <button
-        className='bg-accent text-primary uppercase w-full cursor-pointer rounded-md py-1 px-2 text-center my-3'
+        className='bg-accent text-primary uppercase w-full cursor-pointer rounded-md py-1 px-2 text-center mt-auto mb-3'
         onClick={() => addItemToCart(id)}
       >
         Add to cart
       </button>
+      </div>
     </div>
   )
 }
