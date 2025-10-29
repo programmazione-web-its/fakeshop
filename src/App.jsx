7eb6a5ec3b7e45ec2ddca07dc61662cce2e2bdf7
analayze <annalisa@antonioli.dev>
2025-10-29 09:35:55 +0100

    update shop app

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,25 @@ import CartContextProvider from './providers/CartContextProvider'
 import Header from './components/Header'
 import Shop from './components/Shop'
 
-
 function App() {
-
   const [products, setProducts] = useState([])
-
-
+  const [error, setError] = useState()
+  const [isLoading, setIsLoading] = useState()
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
-    .then((res) => res.json())
-    .then((json) => {
-      console.log("content is fetching")
-      setProducts(json)
-    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('content is fetching')
+        setProducts(json)
+      })
   }, [])
-
 
   return (
     <>
       <CartContextProvider>
         <Header />
-        <Shop products={products} />
-        1 2 3 4
+        <Shop products={products} isLoading={isLoading} error={error} />
       </CartContextProvider>
       <footer className='bg-secondary  rounded-t-xl mt-20'>
         <div className='container text-primary text-md text-center py-20 '>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,22 @@
 import Product from './Product'
-export default function Shop({ products }) {
+export default function Shop({ products, isLoading, error }) {
   return (
-    <div className='grid grid-cols-3 gap-8 container my-10'>
-      {products?.map((el) => (
-        <Product key={el.id} product={el} />
-      ))}
-    </div>
+    <>
+      {isLoading && (
+        <p className='h-[50vh] flex justify-center items-center'>Loading....</p>
+      )}
+      {error && (
+        <p className='h-[50vh] flex justify-center items-center'>
+          ❌ {error.message}
+        </p>
+      )}
+      {products && (
+        <div className='grid grid-cols-3 gap-8 container my-10'>
+          {products?.map((el) => (
+            <Product key={el.id} product={el} />
+          ))}
+        </div>
+      )}
+    </>
   )
 }
